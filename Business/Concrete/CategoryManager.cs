56e3b6c91e6cdb54208782a25c59e6831263fd36
atacanguzelkaya <atacangzlky@gmail.com>
2023-11-19 14:10:21 +0300

    Update

--- Business/Concrete/CategoryManager.cs
+++ Business/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -13,19 +14,21 @@ namespace Business.Concrete
     {
         ICategoryDal _categoryDal;
 
-        public CategoryManager(ICategoryDal categorydal)
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            _categoryDal = categorydal;
+            _categoryDal = categoryDal;
         }
 
-        public List<Category> GetAll()
+        public IDataResult<List<Category>> GetAll()
         {
-            return _categoryDal.GetAll();
+            //İş kodları
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetAll());
         }
-        //Select * from Categories where categoryId = 3
-        public List<Category> GetAllById(int categoryId)
+
+        //Select * from Categories where CategoryId = 3
+        public IDataResult<Category> GetById(int categoryId)
         {
-            return _categoryDal.GetAll(c=>c.CategoryId == categoryId);
+            return new SuccessDataResult<Category>(_categoryDal.Get(c => c.CategoryId == categoryId));
         }
     }
 }

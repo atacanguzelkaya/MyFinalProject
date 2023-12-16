41afcde409dab7664fc3114c7d993e868c86940b
atacanguzelkaya <atacangzlky@gmail.com>
2023-12-16 16:45:15 +0300

    Added JWT, Hashing, Encyption, Token, User and Auth

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        List<OperationClaim> GetClaims(User user);
+    }
+}
